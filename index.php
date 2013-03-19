dff13350ca82e97383be6572dfcba44b4315b83a
root <root@beachkid.(none)>
2013-03-19 16:18:56 +0000

    upgraded drupal; cleaned out a load of cruft

--- index.php
+++ index.php
@@ -18,4 +18,5 @@ define('DRUPAL_ROOT', getcwd());
 
 require_once DRUPAL_ROOT . '/includes/bootstrap.inc';
 drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
+echo '<h1>---------------- more cruft ------------------</h1>';
 menu_execute_active_handler();
